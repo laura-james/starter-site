0e124cfd439602750b50d2990f054814c38bbc8f
laura-james <lollypopstar70@gmail.com>
2025-07-28 12:32:57 +0100

    starter site

---
+++ index.php
@@ -0,0 +1,62 @@
+<!-- header -->
+ <html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Course Pal - find computing and tech courses easily</title>
+    <!-- Link to External Stylesheet -->
+    <link rel="stylesheet" href="css/styles.css">
+  </head>
+  <body>
+    <header>
+      <div class="header-left">
+        <a href="index.php" class="logolink">
+            <img src="images/logo.svg" width="100" alt="Course Pal Logo">
+            <span class="sitename">Course Pal</span>
+        </a>
+    </div>
+    <div class="header-right">
+        <!-- logged in welcome message to go here -->
+        Welcome username
+        <nav>
+            <ul>
+                <li><a href="courses.php">All Courses</a></li>
+                <li><a href="account.php">My Account</a></li>
+                <li><a href="logout.php">Logout</a></li>
+                <li><a href="index.php">Login</a> </li>
+                <li><a href="register.php">Register</a></li>
+                <li><a href="admin.php">Admin</a></li>
+                <li><a href="reports.php">Reports</a></li>
+            </ul>
+        </nav>
+    </div>
+  </header>
+  <div class="container">
+    <section class="homelayout">
+        <div class="text">
+            <h1>Welcome to Course Pal</h1>
+            <p> Unlock your future with hands-on tech training. Whether you're starting out or looking to upskill, our expert-led courses in coding, data science, cybersecurity, and cloud computing are designed to help you succeed in the digital world. Learn at your own pace, build real-world projects, and gain the confidence to thrive in today's tech-driven job market.</p>
+            <p>With a diverse range of courses for all skill levels, we cover everything from web development and artificial intelligence to networking, digital marketing, and IT support. Whether you're a complete beginner, a career switcher, or a professional looking to advance, our flexible learning options mean there's something for everyone. Our curriculum evolves with the tech industry, ensuring you're always learning the most up-to-date and relevant skills.</p>
+            
+            Please login to use the site
+            
+            <h2>Login</h2>
+            <!-- login form to go here -->
+            Or register here <a class = 'btn' href="register.php">Register</a>
+        
+        </div>
+        <div class="image">
+            <!-- featured courses to go here -->
+        </div>
+    </section>
+        
+    <!-- footer -->
+    
+    </div>
+    <footer>
+        <div class="left">&copy; CoursePal 2025 by Raspberry Pi Foundation</div>
+        <div class="right">Contact Us: info@coursepal.com +44 01234 567 8910</div>
+    </footer>
+   
+  </body>
+</html>
---
+++ reports.php
@@ -0,0 +1,49 @@
+<!-- header -->
+ <html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Course Pal - find computing and tech courses easily</title>
+    <!-- Link to External Stylesheet -->
+    <link rel="stylesheet" href="css/styles.css">
+  </head>
+  <body>
+    <header>
+      <div class="header-left">
+        <a href="index.php" class="logolink">
+            <img src="images/logo.svg" width="100" alt="Course Pal Logo">
+            <span class="sitename">Course Pal</span>
+        </a>
+      </div>
+      <div class="header-right">
+          <!-- logged in welcome message to go here -->
+          Welcome username
+          <nav>
+              <ul>
+                  <li><a href="courses.php">All Courses</a></li>
+                  <li><a href="account.php">My Account</a></li>
+                  <li><a href="logout.php">Logout</a></li>
+                  <li><a href="index.php">Login</a> </li>
+                  <li><a href="register.php">Register</a></li>
+                  <li><a href="admin.php">Admin</a></li>
+                  <li><a href="reports.php">Reports</a></li>
+              </ul>
+          </nav>
+      </div>
+    </header>
+    <div class="container">
+      <section>
+        <h1>Courses Reports</h1>
+        <!-- list of courses to appear here -->
+      </section>
+          
+      <!-- footer -->
+      
+    </div>
+    <footer>
+        <div class="left">&copy; CoursePal 2025 by Raspberry Pi Foundation</div>
+        <div class="right">Contact Us: info@coursepal.com +44 01234 567 8910</div>
+    </footer>
+   
+  </body>
+</html>
